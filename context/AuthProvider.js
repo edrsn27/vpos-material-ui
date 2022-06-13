739b5211096ebaed91fe6741061daf623803d283
ederson <edersonvillegas50@gmail.com>
2022-06-14 07:09:47 +0800

    initial firebase config

---
+++ context/AuthProvider.js
@@ -0,0 +1,62 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+import { auth, db } from "../firebase-config";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from "firebase/auth";
+
+const AuthContext = createContext();
+
+import { ref, set } from "firebase/database";
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export default function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const signup = async (email, password, data) => {
+    return await createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signin = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signout = () => {
+    auth.signOut();
+  };
+
+  const forgotpassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        user.getIdTokenResult().then((idTokenResult) => {
+          user.admin = idTokenResult.claims.admin;
+        });
+      }
+
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    loading,
+    signup,
+    signin,
+    signout,
+    forgotpassword,
+    writeUserData,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import AuthProvider from "../context/AuthProvider";
+function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+}
+
+export default MyApp;
